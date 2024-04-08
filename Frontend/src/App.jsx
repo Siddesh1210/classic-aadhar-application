0f6216f4768dda5edf4a1f77143d0fdd34b7d3bf
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2024-04-08 19:52:50 +0530

    Addition of error route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route,useNavigate} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import DisplayForm from "./components/DisplayForm";
 import Login from "./components/Login";
@@ -7,6 +7,7 @@ import AllUser from "./components/AllUser";
 import PrivateComponent from "./components/PrivateComponent";
 
 const App = () => {
+    const navigate=useNavigate();
   return (
     <>
       <Navbar />
@@ -17,6 +18,7 @@ const App = () => {
         </Route>
         <Route path="/" element={<DisplayForm />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<p className="text-center mt-3 fs-5" style={{letterSpacing:"1px"}}>Oops...Page your are looking for does not exist!!! Please return to <span onClick={()=>navigate("/")} style={{color:"blue",cursor:"pointer"}}>Home Page</span></p>}/>
       </Routes>
     </>
   );
