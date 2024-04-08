e31cfaf140c271aec08d650caa4d9ccea55d4712
Siddesh Jaiswal <99405624+Siddesh1210@users.noreply.github.com>
2024-04-09 01:09:43 +0530

    Update Login Button

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
       const result = await response.json();
 
       console.log(result);
-
+        setLoginText("Login")
       setDisplayText(result.message);
 
       if (result.isOk) {
